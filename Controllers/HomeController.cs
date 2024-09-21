99e0321371340559f8b930dd6a14b823f0454f1c
siavsh <Ali.besharati@hotmail.com>
2024-09-21 11:35:01 +0330

    Add action btn

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,18 +43,12 @@ public class HomeController : Controller
     }
 
 
-    public IActionResult privacy()
-    {
-        return View();
-    }
-
-
-
     public IActionResult Privacy()
     {
         return View();
     }
 
+   
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
---
+++ ViewComponents/FooterNavViewComponent.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace divar.ViewComponents
+{
+    public class FooterNavViewComponent : ViewComponent
+    {
+        public List<FooterNav> footerItems = [];
+
+        public FooterNavViewComponent()
+        {
+            footerItems = FooterNav.Items();
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = footerItems;
+            return await Task.FromResult((IViewComponentResult)View("FooterNavView", model));
+        }
+    }
+
+
+    public class FooterNav
+    {
+        public string Title { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+
+        public static List<FooterNav> Items()
+        {
+            List<FooterNav> itemList =
+            [
+                new FooterNav() { Title = "تماس با ما", Action = "ContactUs", Controller = "Home" },
+                new FooterNav() { Title = "درباره ما", Action = "AboutUs", Controller = "Home" },
+                new FooterNav() { Title = "پایگاه دانش", Action = "KnowledgeBase", Controller = "Home" },
+                new FooterNav() { Title = "استعلام", Action = "Index", Controller = "Inquiry" },
+            ];
+            return itemList;
+        }
+    }
+}
