00c0ad9ff7951acacb71da42f8e1f3fd9df861e1
siavsh <Ali.besharati@hotmail.com>
2024-09-29 15:06:06 +0330

    Add Migrations

---
+++ Migrations/20240929113527_ReviewStatuse.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace divar.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReviewStatuse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PostToken",
+                table: "Reservations",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReviewStatus",
+                table: "Reservations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostToken",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewStatus",
+                table: "Reservations");
+        }
+    }
+}
