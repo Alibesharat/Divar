b8d37cd128a4e0eeb7f1186ea003c04fae8851d5
siavsh <Ali.besharati@hotmail.com>
2024-09-30 20:54:08 +0330

    Add PostTitle

---
+++ Migrations/20240930172317_Add postTile.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace divar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddpostTile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PostTitle",
+                table: "Reservations",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostTitle",
+                table: "Reservations");
+        }
+    }
+}
